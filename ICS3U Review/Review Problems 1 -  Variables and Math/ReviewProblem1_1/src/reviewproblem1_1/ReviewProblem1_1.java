d5436ad7cc7c72c0bf7e56cac570275c6f2c6a62
lukra1175 <lukra1175@8T71C62.Student.UGDSB.ED>
2020-09-21 14:47:42 -0400

    Adding review problem 1

---
+++ ICS3U Review/Review Problems 1 -  Variables and Math/ReviewProblem1_1/src/reviewproblem1_1/ReviewProblem1_1.java
@@ -0,0 +1,27 @@
+/*
+ * Lukas Krampiz
+ * Sep 21, 2020
+ * add 3 and 10
+ */
+package reviewproblem1_1;
+
+/**
+ *
+ * @author lukra1175
+ */
+public class ReviewProblem1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 3;
+        int num2 = 10;
+        int sum;
+        sum = num1 + num2;
+        
+        System.out.println(num1 + " plus " + num2 + " equals " + sum);
+    }
+    
+}
