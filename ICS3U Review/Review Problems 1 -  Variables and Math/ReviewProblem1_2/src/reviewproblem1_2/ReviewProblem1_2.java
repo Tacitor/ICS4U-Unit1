92d2696a2909b3e1d195849b271d4de7ef11db89
lukra1175 <lukra1175@8T71C62.Student.UGDSB.ED>
2020-09-21 14:10:08 -0400

    Adding all the review problems from Part 1.

---
+++ ICS3U Review/Review Problems 1 -  Variables and Math/ReviewProblem1_2/src/reviewproblem1_2/ReviewProblem1_2.java
@@ -0,0 +1,27 @@
+/*
+ * Lukas Krampitz
+ * Sep 20, 2020
+ * Calculate and display the difference between 14 and 6.
+ */
+package reviewproblem1_2;
+
+/**
+ *
+ * @author Lukas
+ */
+public class ReviewProblem1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 14;
+        int num2 = 6;
+        int diff;
+        diff = num1 - num2;
+        
+        System.out.println(num1 + " - " + num2 + " = " + diff);
+    }
+    
+}
