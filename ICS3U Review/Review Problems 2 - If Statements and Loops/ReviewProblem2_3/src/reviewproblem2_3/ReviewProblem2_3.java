f753f94ec8b9c51960a84eb2f759f0c556919561
Lukas <Lukas@DESKTOP-TQJA52Q>
2020-09-21 20:56:43 -0400

    Add problem 3

--- ICS3U Review/Review Problems 2 - If Statements and Loops/ReviewProblem2_3/src/reviewproblem2_3/ReviewProblem2_3.java
+++ ICS3U Review/Review Problems 2 - If Statements and Loops/ReviewProblem2_3/src/reviewproblem2_3/ReviewProblem2_3.java
@@ -5,6 +5,9 @@
  */
 package reviewproblem2_3;
 
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lukas
@@ -16,6 +19,61 @@ public class ReviewProblem2_3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Formats
+        DecimalFormat money = new DecimalFormat("$#,##0.00");
+        
+        //vars
+        boolean inputActive = true;
+        boolean validInput;
+        int[] tickets = new int[3];
+        String[] ticketTypes = {"Child","Adult","Senior"};
+        double[] ticketPrices = {6,7.5,6.5};
+        int inputAge = 0;
+        
+        double finalCost = 0;
+        String output = "==========Ticket Sales==========";
+
+        //input
+        while (inputActive) {
+            validInput = false;
+            //error trap for invalid entries
+            while (validInput == false) {                
+                try {
+                    inputAge = Integer.parseInt(JOptionPane.showInputDialog("Please enter the patron's age (999 to finish): "));
+                    validInput = true;
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "That is not a number please try again", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+
+            //determin the trype of ticket
+            if (inputAge == 999) {
+                //if the sentinel is found exit the loop
+                inputActive = false;
+            } else if (inputAge >= 0 && inputAge <= 12) {
+                tickets[0]++; //the $6 ticket
+            } else if (inputAge > 12 && inputAge <= 64) {
+                tickets[1]++; //the $7.50 ticket
+            } else if (inputAge > 65) {
+                tickets[2]++; //the $6.50 ticket
+            } else {
+                JOptionPane.showMessageDialog(null, "Sorry that does not seem to be a valid age", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+        //logic
+        for (int i = 0; i < 3; i++) {
+            //add to the bill
+            output += "\n" + tickets[i] + "x " + ticketTypes[i] + " tickets at " + money.format(tickets[i] * ticketPrices[i]);
+            //add to the total
+            finalCost += tickets[i] * ticketPrices[i];
+        }
+        
+        //output
+        //append the final line
+        output += "\nFinal overal cost of: " + money.format(finalCost);
+        
+        System.out.println(output);
     }
-    
+
 }
