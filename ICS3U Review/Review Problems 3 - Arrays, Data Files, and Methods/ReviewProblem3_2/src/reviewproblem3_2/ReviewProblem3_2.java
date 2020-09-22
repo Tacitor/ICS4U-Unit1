6a74364039e22df6274062c3589d50effa1843ac
Lukas <Lukas@DESKTOP-TQJA52Q>
2020-09-22 12:09:11 -0400

    Add Review Part 3 problem 2

---
+++ ICS3U Review/Review Problems 3 - Arrays, Data Files, and Methods/ReviewProblem3_2/src/reviewproblem3_2/ReviewProblem3_2.java
@@ -0,0 +1,61 @@
+/*
+ * Lukas Krampitz
+ * Sep 22, 2020
+ * Read 20 names from a file and as you get the names, store them in an array.  Make sure to include your own name at least twice. Then preform tasks with the names.
+ */
+package reviewproblem3_2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lukas
+ */
+public class ReviewProblem3_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //vars
+        String[] names = new String[20];
+        int numMyName = 0;
+        int myNamePos = -1; //set to -1 as deflausf if none found yet
+        
+        //read in the data file and create chart
+        try {
+            //load the data file
+            File file = new File("src\\reviewproblem3_2\\names.txt");
+            //create the scanner
+            Scanner scanner = new Scanner(file);
+            
+            //read in the 20 names
+            for (int i = 0; i < 20; i++) {
+                //collect the record
+                names[i] = scanner.nextLine();                
+            }
+            
+            //go through the array an preform some analysis
+            for (int i = 0; i < 20; i++) {
+                //check for my name
+                if (names[i].equalsIgnoreCase("lukas")) {
+                    numMyName++;
+                    if (myNamePos == -1) {
+                        myNamePos = i;
+                    }
+                }
+            }
+            
+            System.out.println("My name of Lukas was found: " + numMyName + " times"
+                    + "\nThe first occurence of my name was in position: " + myNamePos);
+            
+        } catch (FileNotFoundException e) {
+            //display the error
+            System.out.println("Error: " + e);
+        }
+    }
+    
+}
