0e26535631bf8fe55f8ac85cf8b03c1403bab45d
Lukas <Lukas@DESKTOP-TQJA52Q>
2020-09-21 20:13:22 -0400

    Add problem 2 and part of 3

---
+++ ICS3U Review/Review Problems 2 - If Statements and Loops/ReviewProblem2_3/src/reviewproblem2_3/ReviewProblem2_3.java
@@ -0,0 +1,21 @@
+/*
+ * Lukas Krampitz
+ * Sep 21, 2020
+ * Create a program called ManyTickets. Your program should allow users to input their age. The computer should respond with the appropriate price for a ticket.
+ */
+package reviewproblem2_3;
+
+/**
+ *
+ * @author Lukas
+ */
+public class ReviewProblem2_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
